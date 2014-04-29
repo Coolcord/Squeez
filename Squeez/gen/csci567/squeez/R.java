a1617f7b0b0b8b3b5a992f4c277d0df714306856
Coolcord <coolcord24@gmail.com>
2014-04-29 15:46:13 -0700

    Added more padding and changed the text size of the current directory bar

--- Squeez/gen/csci567/squeez/R.java
+++ Squeez/gen/csci567/squeez/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int file=0x7f020000;
         public static final int folder=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int squeez=0x7f020003;
+        public static final int music=0x7f020003;
+        public static final int squeez=0x7f020004;
     }
     public static final class id {
         public static final int LayoutDirectoryLocation=0x7f080005;
